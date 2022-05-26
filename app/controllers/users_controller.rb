436198ebe5b39b5e133cc3bc7fb149827fc7f29a
Michael Franco <franco.luis.michael@gmail.com>
2022-05-25 22:34:11 -0500

    commit tree 10d048cea922ecb7930ae3d71892aa2efe2597cb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        # render json: Item.find_by(params[:id]), status: :ok
+        render json: Item, include: :reviews
+    
+    end
 end
